22ca5165e86f2a3d2219f1a99a74e33f82ec675b
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2024-11-06 16:32:10 +0530

    first commit

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+mod models;
+mod services;
+mod routes;
+
+use actix_web::{get, web::Data, App, HttpResponse, HttpServer, Responder};
+use routes::{booking_route::{cancel_booking, create_booking, get_bookings}, dog_route::create_dog, owner_route::create_owner};
+use services::db::Database;
+
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello Mohammed Maaz")
+}
+
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let db = Database::init().await;
+    let db_data = Data::new(db);
+
+    HttpServer::new(move || {
+        App::new().app_data(db_data.clone())
+        .service(hello)
+        .service(create_booking)
+        .service(create_dog)
+        .service(create_owner)
+        .service(get_bookings)
+        .service(cancel_booking)
+    }).bind(("localhost", 5000))?.run().await
+}
